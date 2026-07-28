a2e9516f1fba3bbdbb0acb4c4378e2ea833c7ca7
Jae-Min Jo <44039707+91jaeminjo@users.noreply.github.com>
2026-07-28 14:32:45 -0500

    chore: version 0.97.2+75 (#470)

--- lib/version.dart
+++ lib/version.dart
@@ -2,4 +2,4 @@
 ///
 /// Generated from pubspec.yaml. Run `dart run tool/update_version.dart` after
 /// changing the version in pubspec.yaml.
-const String soliplexVersion = '0.97.1+74';
+const String soliplexVersion = '0.97.2+75';
